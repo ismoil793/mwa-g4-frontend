90393f0a8f31b06f8aff667eeb945375965aee2b
birukyemane <bir_yem@yahoo.com>
2023-03-18 10:26:02 -0500

    - create myAutomobiles component

--- src/app/automobile/automobile.module.ts
+++ src/app/automobile/automobile.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { AutoDetailComponent } from './auto-detail/auto-detail.component';
+import { MyAutomobilesComponent } from './my-automobiles/my-automobiles.component';
 
 const routes: Routes = [
   { path: '', component: ListComponent },
@@ -10,10 +11,11 @@ const routes: Routes = [
     path: 'auto-detail',
     component: AutoDetailComponent,
   },
+  { path: 'myAutomobiles', component: MyAutomobilesComponent },
 ];
 
 @NgModule({
-  declarations: [AutoDetailComponent],
+  declarations: [AutoDetailComponent, MyAutomobilesComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class AutomobileModule {}
---
+++ src/app/automobile/my-automobiles/my-automobiles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-automobiles',
+  templateUrl: './my-automobiles.component.html',
+  styleUrls: ['./my-automobiles.component.css']
+})
+export class MyAutomobilesComponent {
+
+}
