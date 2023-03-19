58b4037bd0149cbe5c6f4321df3ff6c3513a8e24
Tushar Ghosh <tusharghosh09006@gmail.com>
2023-03-19 17:24:50 -0500

    fix the design of login and signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -56,6 +56,6 @@ export class LoginComponent {
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -62,6 +62,6 @@ export class SignupComponent {
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 }
